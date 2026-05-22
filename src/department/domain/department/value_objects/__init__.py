9c1c8efd0f53f4498f1e3228b8cf2e313b225774
batmaxcom <batmanovv@kamaz.ru>
2026-05-22 11:29:17 +0300

    feat(DepartmentAPI): Add next layers(application, infrastructure, presentation, entrypoint)

---
+++ src/department/domain/department/value_objects/__init__.py
@@ -0,0 +1,8 @@
+from .department_name import DepartmentName
+from .department_id import DepartmentId
+
+
+__all__ = (
+    "DepartmentId",
+    "DepartmentName",
+)
---
+++ src/department/domain/employee/value_object/__init__.py
@@ -0,0 +1,11 @@
+from .employee_id import EmployeeId
+from .employee_name import EmployeeName
+from .position import Position
+
+
+
+__all__ = (
+    "EmployeeId",
+    "EmployeeName",
+    "Position"
+)
